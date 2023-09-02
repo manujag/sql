543c07cb32d38dedfd6ca14bf7d4c60f2d2ebf04
manujag <mjmanu91@gmail.com>
2023-09-02 23:50:46 +0530

    updated comments

--- postgreSQL/cross_tab.sql
+++ postgreSQL/cross_tab.sql
@@ -1,15 +1,19 @@
 SET SEARCH_PATH TO sales_report;
 
+
+-- check the table data
 SELECT * FROM sellers_item_qty;
 
 
+
+-- cross tab query
 SELECT *
 FROM crosstab(
-  'select sellersid, itemcode, SUM(qty) as qty 
-   from sellers_item_qty
-GROUP BY sellersid, itemcode
-   order by sellersid, itemcode',
+  'SELECT sellersid, itemcode, SUM(qty) AS qty FROM sellers_item_qty
+GROUP BY sellersid, itemcode ORDER BY sellersid, itemcode',
    'SELECT DISTINCT itemcode FROM sellers_item_qty ORDER BY 1 LIMIT 5')
 AS ct(seller_id text, a1_total INTEGER, a2_total INTEGER, a3_total INTEGER, a4_total INTEGER, a5_total INTEGER);
+
+
 
 
